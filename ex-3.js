6e0b96c111067104ba70d84f74dab2f94288dead
pichaya kumdee <otakusas@hotmail.com>
2024-08-03 19:12:57 +0700

    update:Assignment

--- ex-3.js
+++ ex-3.js
@@ -3,27 +3,18 @@
 // Start coding here
 const employee = {
   name: "John Doe",
-  age: 18,
-  address: {
-    street: "123 Main Street",
+  age: 18,}
+ const address =  {
+   street: "123 Main Street",
     city: "Cityville",
     states: "Stateville",
     country: "Countryland",
     postalCode: "12345",
-  },
-};
+  }
 
-console.log("Employee Name: " + employees.name);
+
+console.log("Employee name: "+employee.name);
 console.log("Employee Age: " + employee.age);
-console.log(
-  "Employee Address: " +
-    employee.address.street +
-    ", " +
-    employee.address.city +
-    ", " +
-    employee.address.state +
-    ", " +
-    employee.address.country +
-    " " +
-    employee.adress.postalCode
+console.log("Employee Address: " + address.street +","+ address.city + ","+
+address.states+ "," + address.country+ "," + address.postalCode
 );
